972d60e6af9bcff451d599e45dc2421b6ba19504
Allison Howard <alliemhow@gmail.com>
2019-10-31 22:07:00 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,14 @@
 def position_taken?(board, index)
+<<<<<<< HEAD
 
   if board[index] ==  "X" || board[index] == "O"
     return true
 
   elsif board[index] == " " ||  board[index] == "" ||  board[index] == nil
     return false
+=======
+  if board[index] == ( "X" || "O" ) && !( " " || "" || nil )
+    return true
+>>>>>>> aeb881c32d2f1c1ccba459517c74ed68523bf2e0
   end
 end
